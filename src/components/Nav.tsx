323062be7b0fabf53fa23f828817eeff8642f722
于邦飞 <528154320@qq.com>
2024-03-26 10:35:53 +0800

    添加选择工作目录

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,10 +7,15 @@ const {ipcRendererSend} = (window as any).electron
     ipcRendererSend('minimize')
   }
 
+  function handleOpenFolder() {
+    ipcRendererSend('open-folder')
+  }
+
 export default function Nav(){
     return (
         <>
         <div className='flex flex-row h-3 item w-full justify-end'>
+            <div className='justify-self-end h-full pr-1.5 pl-1.5 items-center flex cursor-pointer' onClick={()=>handleOpenFolder()}>e</div>
             <div className='justify-self-end h-full pr-1.5 pl-1.5 items-center flex cursor-pointer' onClick={()=>handleMinimize()}>-</div>
             <div className='justify-self-end h-full pr-1.5 pl-1.5 items-center flex cursor-pointer' onClick={()=>handleClose()}>x</div>
         </div>
