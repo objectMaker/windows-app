f9fc9f1c1fdf5daef17cfd780c52312fce8bf2c5
于邦飞 <528154320@qq.com>
2024-03-27 19:27:12 +0800

    格式化一些东西

--- src/components/MediaList.tsx
+++ src/components/MediaList.tsx
@@ -1,18 +1,18 @@
 export type FileListItem = {
-    path:string,
-    size:string,
-    uid:string,
-    basename:string,
-    extname:string,
-    name:string
+    path: string,
+    size: string,
+    uid: string,
+    basename: string,
+    extname: string,
+    name: string
 }
-export default function({fileList}:{
-    fileList:FileListItem[]
-}){
+export default function ({ fileList }: {
+    fileList: FileListItem[]
+}) {
     return <div className="h-[100px] overflow-y-scroll">
         {
-            fileList.map((file)=>
-            <div className="h-[20px] text-yellow-400">{file.name}</div>
+            fileList.map((file) =>
+                <div className="h-[20px] text-yellow-400">{file.name}</div>
             )
         }
     </div>
