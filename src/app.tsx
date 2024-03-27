44e9c8bcf0e80438c5384252077dde60f64bad3e
于邦飞 <528154320@qq.com>
2024-03-27 19:18:38 +0800

    add player

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import Nav from './components/Nav';
 import './index.css'
+import Player from './components/Player';
 const root = createRoot(document.getElementById('root'));
 root.render(<>
-    <Nav/>
+    <Nav />
+    <Player />
 </>);
