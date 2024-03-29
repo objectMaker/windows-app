c75c8b13451f77b8308992b0c36582e42942d168
于邦飞 <528154320@qq.com>
2024-03-29 13:39:38 +0800

    添加播放控制

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,5 @@
-import { useContext,useRef,useState,useEffect} from "react";
-import ReactPlayer,{ReactPlayerProps} from 'react-player'
+import { useContext, useRef, useState, useEffect, useCallback } from "react";
+import ReactPlayer, { ReactPlayerProps } from 'react-player'
 import duration from 'dayjs/plugin/duration'
 import dayjs from 'dayjs'
 import { GlobalContext } from "../context";
@@ -7,41 +7,56 @@ dayjs.extend(duration);
 
 
 export default function player() {
-    const [reactPlayerProps,setReactPlayerProps] = useState<ReactPlayerProps>({
-        playing:false,
-        loop:true,
-        onProgress:handleProgress,
-        progressInterval:100,
-    })
-    const [playedTime,setPlayedTime] = useState('00:00:00')
-    const [totalTime,setTotalTime] = useState('00:00:00')
-    const [percent,setPercent] = useState('0%')
-    const [isMoveProgressIndicator,setIsMoveProgressIndicator] = useState(false);
+    const [isMoveProgressIndicator, setIsMoveProgressIndicator] = useState(false);
+    const handleProgress = useCallback((params: any) => {
+        setTotalTime(dayjs.duration(params.loadedSeconds, 'second').format('HH:mm:ss'))
+        setTotalSecondsTime(params.loadedSeconds)
+        //如果是在拖动，进度条和时间只能根据手拖动的位置进行计算
+        if (!isMoveProgressIndicator) {
+            setPlayedTime(dayjs.duration(params.playedSeconds, 'second').format('HH:mm:ss'))
+            setPlayed(params.played)
+        }
+    }, [isMoveProgressIndicator])
+    const [playedTime, setPlayedTime] = useState('00:00:00')
+    const [totalTime, setTotalTime] = useState('00:00:00')
+    const [secondsTime, setTotalSecondsTime] = useState(0)
+    const [played, setPlayed] = useState(0)
+
     const progressIndicatorTotalRef = useRef<HTMLDivElement>()
     const progressIndicatorCurrentRef = useRef<HTMLDivElement>()
     const playerRef = useRef<ReactPlayer>(null);
+    const [reactPlayerProps, setReactPlayerProps] = useState<ReactPlayerProps>({
+        playing: false,
+        loop: true,
+        progressInterval: 100,
+    })
     //进度条长度
-    let progressIndicatorLength:number;
-    function handleProgress(params: any) {
-        setTotalTime(dayjs.duration(params.loadedSeconds,'second').format('HH:mm:ss'))
-        //如果是在拖动，进度条和时间只能根据手拖动的位置进行计算
-        if(!isMoveProgressIndicator){
-            setPlayedTime(dayjs.duration(params.playedSeconds,'second').format('HH:mm:ss'))
-            setPercent(Math.ceil(params.played*10000)/100+'%')
-        }
-      }
-        const {audioLink} = useContext(GlobalContext);
-      function handleUpdateReactPlayerProps(params:Partial<ReactPlayerProps>) {
-        setReactPlayerProps((prevProps)=>({...prevProps,...params}))
-      }
-      function handleIndicatorTotalMouseMove(){}
-      function handleIndicatorTotalMouseLeave(){}
-      function handleIndicatorTotalMouseUp(){}
-      useEffect(()=>{
-        progressIndicatorLength =  progressIndicatorTotalRef.current.getBoundingClientRect().width
-      },[])
+    let progressIndicatorLength: number;
+
+    const { audioLink } = useContext(GlobalContext);
+    function handleUpdateReactPlayerProps(params: Partial<ReactPlayerProps>) {
+        setReactPlayerProps((prevProps) => ({ ...prevProps, ...params }))
+    }
+    function handleIndicatorTotalMouseMove() {
+    }
+    function handleIndicatorTotalMouseLeave() { }
+    function handleIndicatorTotalMouseUp() {
+        setIsMoveProgressIndicator(false)
+        playerRef.current.seekTo(played)
+    }
+    function handleIndicatorTotalMouseDown() {
+        setIsMoveProgressIndicator(true)
+    }
+    function handlePlayedChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setPlayedTime(dayjs.duration(+e.target.value * secondsTime, 'second').format('HH:mm:ss'))
+        setPlayed(+e.target.value)
+    }
+
+    useEffect(() => {
+        progressIndicatorLength = progressIndicatorTotalRef.current.getBoundingClientRect().width
+    }, [])
     return <>
-          {audioLink && <ReactPlayer {...reactPlayerProps} width={0} height={0} ref={playerRef} url={audioLink} />}
+        {audioLink && <ReactPlayer {...reactPlayerProps} width={0} height={0} ref={playerRef} url={audioLink} onProgress={handleProgress} />}
         <div className="relative z-10 p-4">
             <div className="flex w-[41rem] rounded-lg bg-gray-50 shadow-xl shadow-black/5 ring-1 ring-slate-700/10">
                 <div className="flex items-center space-x-4 px-6 py-4">
@@ -52,15 +67,18 @@ export default function player() {
                         </path>
                     </svg>
                     <svg className="h-10 w-10 flex-none" fill="none">
-                        <circle cx="20" cy="20" r="20" fill="#0F172A" onClick={()=>handleUpdateReactPlayerProps({playing:!reactPlayerProps.playing})}></circle>
+                        <circle cx="20" cy="20" r="20" fill="#0F172A" onClick={() => handleUpdateReactPlayerProps({ playing: !reactPlayerProps.playing })}></circle>
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M13.5 13.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L16.28 27.99c-1.25.687-2.779-.217-2.779-1.643V13.653Z" fill="#fff"></path></svg><svg className="h-6 w-6 flex-none" fill="none"><path d="M16.72 9.97a.75.75 0 1 0 1.06 1.06l-1.06-1.06ZM21 6.75l.53.53a.75.75 0 0 0 0-1.06l-.53.53Zm-3.22-4.28a.75.75 0 1 0-1.06 1.06l1.06-1.06ZM10.5 19.5a.75.75 0 0 0 0-1.5v1.5Zm3.75-4.5a.75.75 0 0 0 0 1.5V15Zm.75.75h.75A.75.75 0 0 0 15 15v.75ZM14.25 21a.75.75 0 0 0 1.5 0h-1.5Zm6-4.5a.75.75 0 0 0 0-1.5v1.5ZM18 15.75V15a.75.75 0 0 0-.75.75H18ZM18 18h-.75c0 .414.336.75.75.75V18Zm0 2.25a.75.75 0 0 0 0 1.5v-1.5Zm-.22-9.22 3.75-3.75-1.06-1.06-3.75 3.75 1.06 1.06Zm3.75-4.81-3.75-3.75-1.06 1.06 3.75 3.75 1.06-1.06ZM2.25 12.75A6.75 6.75 0 0 0 9 19.5V18a5.25 5.25 0 0 1-5.25-5.25h-1.5ZM9 6a6.75 6.75 0 0 0-6.75 6.75h1.5C3.75 9.85 6.1 7.5 9 7.5V6Zm0 1.5h12V6H9v1.5Zm0 12h1.5V18H9v1.5Zm5.25-3H15V15h-.75v1.5Zm0-.75V21h1.5v-5.25h-1.5Zm6-.75H18v1.5h2.25V15Zm-3 .75V18h1.5v-2.25h-1.5Zm.75 3h1.5v-1.5H18v1.5Zm1.5 1.5H18v1.5h1.5v-1.5Zm.75-.75a.75.75 0 0 1-.75.75v1.5a2.25 2.25 0 0 0 2.25-2.25h-1.5Zm-.75-.75a.75.75 0 0 1 .75.75h1.5a2.25 2.25 0 0 0-2.25-2.25v1.5Z" fill="#64748B"></path></svg></div><div className="flex flex-auto items-center border-l border-slate-200/60 pl-6 pr-4 text-[0.8125rem] leading-5 text-slate-700">
                     <div>{playedTime}</div>
                     <div className="ml-4 flex flex-auto rounded-full bg-slate-100" ref={progressIndicatorTotalRef} onMouseMove={handleIndicatorTotalMouseMove}
-                    onMouseLeave={handleIndicatorTotalMouseLeave}
-                    onMouseUp={handleIndicatorTotalMouseUp}
+                        onMouseLeave={handleIndicatorTotalMouseLeave}
+                        onMouseUp={handleIndicatorTotalMouseUp}
+                        onMouseDown={handleIndicatorTotalMouseDown}
                     >
-                        <div className={`h-2 w-0 flex-none rounded-l-full rounded-r-[1px] bg-indigo-600`} style={{width:percent}} ref={progressIndicatorCurrentRef}></div>
-                        <div className="-my-[0.3125rem] ml-0.5 h-[1.125rem] w-1 rounded-full bg-indigo-600"></div>
+                        <input onMouseUp={handleIndicatorTotalMouseUp}
+                            onMouseDown={handleIndicatorTotalMouseDown} type="range" min="0" max="0.999999" step="any" value={played} className="w-full" onChange={handlePlayedChange}></input>
+                        {/* <div className={`h-2 w-0 flex-none rounded-l-full rounded-r-[1px] bg-indigo-600`} style={{ width: percent }} ref={progressIndicatorCurrentRef}></div>
+                        <div className="-my-[0.3125rem] ml-0.5 h-[1.125rem] w-1 rounded-full bg-indigo-600"></div> */}
                     </div>
                     <div className="ml-4">{totalTime}</div>
                     <svg className="ml-6 h-6 w-6 flex-none" fill="none">
